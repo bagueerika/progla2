0405ba011f853ab1ed4f4f421ae2ac7d445f6891
Erika Marie D. Bague <bague_erika@yahoo.com>
2017-02-04 18:46:18 +0800

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -3,63 +3,67 @@
 #include<string>
 using namespace std;
 
-class student{
+class person{
 	public:
 		string name;
 		int id_num;
+};
+
+class student: public person{
+	public:
 		int num_of_subjects;
 		int num_books;
 };
-class prof{
+
+class prof: public person{
 	public:
-		string name;
-		int id_num;
 		int tin;
 };
-class staff{
+
+class staff: public person{
 	public:
-		string name;
-		int id_num;
 		int sss;
 };
+
 int main(){
+	person per;
 	student stud;
 	cout << "Enter Student Name: ";
-	cin >> stud.name;
+	cin >> per.name;
 	cout << "Enter Student ID Num: ";
-	cin >> stud.id_num;
+	cin >> per.id_num;
 	cout << "Enter Number of Subjects: ";
 	cin >> stud.num_of_subjects;
 	cout << "Enter Number of books: ";
 	cin >> stud.num_books;
 	prof p;
 	cout << "Enter Professor Name: ";
-	cin >> p.name;
+	cin >> per.name;
 	cout << "Enter Professor ID Number: ";
-	cin >> p.id_num;
+	cin >> per.id_num;
 	cout << "Enter Professor TIN Number: ";
 	cin >> p.tin;
 	staff s;
 	cout << "Enter Staff Name: ";
-	cin >> s.name;
+	cin >> per.name;
 	cout << "Enter Staff ID Number: ";
-	cin >> s.id_num;
+	cin >> per.id_num;
 	cout << "Enter Staff SSS Number: ";
 	cin >> s.sss;	
 	
 	cout << "\nOUTPUT: ";
 	cout << "\nStudent: " << endl;
-	cout << "Name: " << stud.name << endl;
-	cout << "ID Number: " << stud.id_num << endl;
+	cout << "Name: " << per.name << endl;
+	cout << "ID Number: " << per.id_num << endl;
 	cout << "Number of Subjects: " << stud.num_of_subjects << endl;
 	cout << "Number of Books: " << stud.num_books << endl;
 	cout << "\nProf: " << endl;
-	cout << "Name: " << p.name << endl;
-	cout << "ID Number: " << p.id_num << endl;
+	cout << "Name: " << per.name << endl;
+	cout << "ID Number: " << per.id_num << endl;
 	cout << "TIN:  " << p.tin << endl;
 	cout << "\nStaff: " << endl;
-	cout << "Name: " << s.name << endl;
-	cout << "ID Number: " << s.id_num << endl;
+	cout << "Name: " << per.name << endl;
+	cout << "ID Number: " << per.id_num << endl;
 	cout << "SSS:  " << s.sss << endl;
-  return 0;
+    return 0;
 }
